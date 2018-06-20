3de27c1e4b616e9dbea029f767587fc8c4d18b2e
Lu Nelson <lunelson@gmail.com>
2018-06-20 18:02:10 +0200

    add debug.js; fix font and frame

--- src/js/debug.js
+++ src/js/debug.js
@@ -3,7 +3,6 @@
 //
 
 if (process.env.NODE_ENV == 'development') {
-
   const body = document.body;
   body.addEventListener('keyup', function(event) {
     if (event.shiftKey == true && event.altKey == true && event.keyCode == 68) {
