469466eb32a5e3f5519e06246540f64258531e7e
Lu Nelson <lunelson@gmail.com>
2018-09-25 14:01:33 +0200

    improved debug, using localStorage to set state

--- src/js/setup/debug.js
+++ src/js/setup/debug.js
@@ -4,11 +4,24 @@
 //
 
 if (process.env.NODE_ENV == 'development') {
+
   const body = document.body;
+
+  const config = JSON.parse(localStorage.getItem('homework')) || { debug: false };
+
+  function update() {
+    localStorage.setItem('homework', JSON.stringify(config));
+    body.classList.toggle('debug', config.debug);
+  }
+
   body.addEventListener('keyup', function(event) {
     if (event.shiftKey == true && event.altKey == true && event.keyCode == 68) {
-      body.classList.toggle('debug');
-      console.log('the key was triggered');
+      config.debug = !config.debug;
+      update();
     }
   });
+
+  update();
 }
+
+// localStorage.setItem('homework', JSON.stringify({ ... }))
