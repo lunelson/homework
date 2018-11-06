acd6fecaf7cb6aaf4f8400a6189af1d1cb604f57
Lu Nelson <lunelson@gmail.com>
2018-11-06 11:54:00 +0100

    split core setup from root and font outputs

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-//  _                                             _           _
-// | |                                           | |         (_)
-// | |__   ___  _ __ ___   _____      _____  _ __| | ________ _ ___
-// | '_ \ / _ \| '_ ` _ \ / _ \ \ /\ / / _ \| '__| |/ /______| / __|
-// | | | | (_) | | | | | |  __/\ V  V / (_) | |  |   <       | \__ \
-// |_| |_|\___/|_| |_| |_|\___| \_/\_/ \___/|_|  |_|\_\      | |___/
-//                                                          _/ |
-//                                                         |__/
+/*
+     _      _
+    | |    | |
+  __| | ___| |__  _   _  __ _
+ / _` |/ _ \ '_ \| | | |/ _` |
+| (_| |  __/ |_) | |_| | (_| |
+ \__,_|\___|_.__/ \__,_|\__, |
+                         __/ |
+                        |___/
+*/
 
-/* eslint-disable */
-// CHECK ENV
-console.log(`the environment is ${process.env.NODE_ENV}`);
-// DO SETUPS
-import './src/js/setup/debug.js';
+const body = document.body;
+
+const config = JSON.parse(localStorage.getItem('homework')) || { debug: false };
+
+function update() {
+  localStorage.setItem('homework', JSON.stringify(config));
+  body.classList.toggle('debug', config.debug);
+}
+
+if (process.env.NODE_ENV == 'development') {
+
+
+  body.addEventListener('keyup', function(event) {
+    if (event.shiftKey == true && event.altKey == true && event.keyCode == 68) {
+      config.debug = !config.debug;
+      update();
+    }
+  });
+
+  update();
+}
