407deefc259ab75cc6edc77bde20a15e4731f9ca
Lu Nelson <lunelson@gmail.com>
2018-12-17 11:41:48 +0100

    havoc refactoring gulp testing workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,85 @@
+/*
+  new GULP sass snapshotting approach
+
+  https://www.npmjs.com/package/gulp-error
+  https://www.npmjs.com/package/gulp-error-handle
+  https://www.npmjs.com/package/gulp-confirm
+  https://www.npmjs.com/package/gulp-prompt
+  https://github.com/geejs/gulp-tap/wiki
+  https://github.com/Freyskeyd/gulp-prompt
+  https://www.npmjs.com/package/gulp-if-else
+  https://github.com/nfroidure/gulp-cond
+  https://github.com/robrich/gulp-if
+  https://medium.com/@boriscoder/catching-errors-on-gulp-js-4682eee2669f
+  https://github.com/OverZealous/lazypipe
+  https://github.com/nfroidure/gulp-cond
+
+
+
+*/
+
 'use strict';
 
 const path = require('path');
-const gulp = require('gulp');
+// const gulp = require('gulp');
 const sass = require('gulp-sass');
 const diff = require('gulp-diff');
 const rename = require('gulp-rename');
-const plumber = require('gulp-plumber');
+// const plumber = require('gulp-plumber');
 
-const sassIncl = path.join(__dirname, 'node_modules');
+const includePaths = [path.join(__dirname, 'node_modules')];
+const sassOptions = { includePaths, outputStyle: 'expanded' };
+
+const { src, dest, watch } = require('gulp');
 
 /*
   gulp 4
   https://github.com/gulpjs/gulp
 */
 
-function work() {
-  return gulp.src('./test/**/*.scss')
-    .pipe(plumber(sass.logError))
-    .pipe(sass({
-      outputStyle: 'expanded',
-      includePaths: [sassIncl]
-    }))
-    .pipe(gulp.dest('./test'));
+const srcGlob = './tests/*.scss';
+
+function render(srcFile = srcGlob) {
+  return src(srcFile)
+    // .pipe(plumber(sass.logError))
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(dest('./test'));
 }
-function test() {
-  return gulp.src('./test/**/*.scss')
-    .pipe(plumber())
-    .pipe(sass({
-      outputStyle: 'expanded',
-      includePaths: [sassIncl]
-    }).on('error', sass.logError))
+
+function compare(srcFile = srcGlob) {
+  return src(srcFile)
+    // .pipe(plumber(sass.logError))
+    .pipe(sass(sassOptions).on('error', sass.logError))
     .pipe(rename({extname: '.css'}))
     .pipe(diff())
     .pipe(diff.reporter({ fail: true }));
 }
-function watch() {
-  return gulp.watch('./**/*.scss', work);
-}
 
-gulp.task('test', test);
-gulp.task('work', work);
-gulp.task('default', watch);
+/*
+  watching
+    create a Map of errors, per file
+    on change -> task
+    - errorHandler, should put errors in the Map
+    - sass render
+    - diff, which will throw error if problem
+    - condition/prompt
+      - does error exist, if so, ask to re-render
+      - re-render snapshot if confirmed
+*/
+
+exports.render = render;
+exports.compare = compare;
+
+exports.default = function() {
+  const watcher = watch(srcGlob, { ignoreInitial: false });
+  watcher.on('add', file => {
+    // if comparison file exists, compare; otherwise render
+    // render(file);
+    fail(file);
+  });
+  watcher.on('change', file => {
+    // compare
+    compare(file);
+  });
+  return watcher;
+};
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const homeworkConfig = JSON.parse(localStorage.getItem('homework')) || { debug: false };
+function setHomeworkConfig() { localStorage.setItem('homework', JSON.stringify(homeworkConfig)); }
+
 /*
      _      _
     | |    | |
@@ -9,24 +12,13 @@
                         |___/
 */
 
-const body = document.body;
-
-const config = JSON.parse(localStorage.getItem('homework')) || { debug: false };
-
-function update() {
-  localStorage.setItem('homework', JSON.stringify(config));
-  body.classList.toggle('debug', config.debug);
-}
-
 if (process.env.NODE_ENV == 'development') {
-
-
-  body.addEventListener('keyup', function(event) {
+  document.body.classList.toggle('debug', homeworkConfig.debug);
+  document.body.addEventListener('keyup', function(event) {
     if (event.shiftKey == true && event.altKey == true && event.keyCode == 68) {
-      config.debug = !config.debug;
-      update();
+      homeworkConfig.debug = !homeworkConfig.debug;
+      document.body.classList.toggle('debug', homeworkConfig.debug);
+      setHomeworkConfig();
     }
   });
-
-  update();
 }
